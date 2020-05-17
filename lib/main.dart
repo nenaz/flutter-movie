ee7f2446124b7b9ec6d280de8dab556984c3b03d
mrnenaz <mrnenaz@gmail.com>
2020-05-17 13:26:01 +0300

    new app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-class MyBody extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Column(
-        children: <Widget>[
-          new Text('Hello world'),
-          new FlatButton(
-            onPressed: () async{
-              const url = 'https:yandex.ru';
-              if (await canLaunch(url)) {
-                await (launch(url));
-              } else {
-                throw('ERROR');
-              }
-            },
-            child: Text('Open'),
-            color: Colors.red,
-            textColor: Colors.white,
-          )
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Local movie',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
       ),
+      home: HomePage(),
     );
   }
 }
 
-void main() {
-  runApp(
-    new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Flutter bar'),
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Any text'),
         ),
-        body: new MyBody(),
-      )
-    )
-  );
+      ),
+    );
+  }
 }
